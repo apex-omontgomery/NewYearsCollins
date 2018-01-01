1b8b939e9bbe9bddb8f00c1174106499eee88606
William Montgomery <10781353+wimo7083@users.noreply.github.com>
2018-01-01 00:08:17 -0700

    fixed errors

--- air2nite.py
+++ air2nite.py
@@ -8,11 +8,11 @@ def convert_struct(input):
     return f"{input.tm_hour}:{input.tm_min}:{input.tm_sec}"
     
 def check_time(input):    
-    return str(input.tm_hour) + str(input.tm_min) + str(input.tm_sec) < '115640'
+    return str(input.tm_hour) + str(input.tm_min) + str(input.tm_sec) >= '115640'
     
 def loop():
     now = localtime()
-    while check_time(now):
+    while not check_time(now):
         print(convert_struct(now), end='\r')
         now = localtime()
         sleep(0.1)
